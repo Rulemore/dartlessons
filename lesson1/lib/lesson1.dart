f6ed1efb746466046827b1032dc4e6deeb59ccd3
VladimirB <belaevvova999@gmail.com>
2023-03-09 21:04:54 +0300

    First homework commit

---
+++ lesson1/lib/lesson1.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+Future getPosts() async {
+  final dio = Dio();
+  try {
+    final response = await dio.get('https://jsonplaceholder.typicode.com/posts');
+    return response.data;
+  } catch (e) {
+    print(e);
+  }
+  return null;
+}
---
+++ lesson1/test/lesson1_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson1/lesson1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
